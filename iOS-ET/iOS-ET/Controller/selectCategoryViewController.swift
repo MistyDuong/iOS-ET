b667b56b8aff325c9993e2ae2f17386dc5537ec1
Qui16 <quydam1609@gmail.com>
2022-05-21 12:25:20 +1000

    displayCategory Update

--- iOS-ET/iOS-ET/Controller/selectCategoryViewController.swift
+++ iOS-ET/iOS-ET/Controller/selectCategoryViewController.swift
@@ -5,11 +5,15 @@
 //  Created by Quy Dam on 17/5/2022.
 //
 
+
 import Foundation
 import UIKit
+import CoreData
 
 
-class selectCategoryViewController: UIViewController, UITextFieldDelegate {
+class selectCategoryViewController: UIViewController, UITextFieldDelegate{
+    
+    
     @IBOutlet weak var allLabel: UILabel!
     @IBOutlet weak var rentLabel: UILabel!
     @IBOutlet weak var groceriesLabel: UILabel!
@@ -17,90 +21,114 @@ class selectCategoryViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var utilitiesLabel: UILabel!
     @IBOutlet weak var entertainmentLabel: UILabel!
     @IBOutlet weak var othersLabel: UILabel!
-    
-    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext;
-    var selectedCategory: String = "name";
-    var username: String = "hello";
+    var balance:[Balance]?
+    let context=(UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext;
+
+    var selectedCategory: String = "";
+    //var name:String = ""
+    var allAmount:Double=0
+    var rentAmount:Double=0
+    var groceriesAmount:Double=0
+    var transportAmount:Double=0
+    var utilitiesAmount:Double=0
+    var entertainmentAmount:Double=0
+    var othersAmount:Double=0
 
     override func viewDidLoad() {
-        super.viewDidLoad();
-        // hide nav bar
-        self.navigationController?.isNavigationBarHidden = true;
-        print("select category - \(username)");
+        super.viewDidLoad()
+        fetchBalance()
+        calculateAmount()
     }
     
+    func fetchBalance(){
+        do{
+            let request = Balance.fetchRequest() as NSFetchRequest<Balance>
+            self.balance = try context.fetch(request)
+            
+        }catch{
+            
+        }
+        DispatchQueue.main.async {
+            
+        }
+    }
     
-    @IBAction func allButton(_ sender: Any) {
+    func calculateAmount(){
+        for item in balance!{
+            
+            if(item.category=="Rent"){
+                rentAmount+=item.amount
+            }
+            if(item.category=="Groceries"){
+                groceriesAmount+=item.amount
+            }
+            if(item.category=="Transport"){
+                transportAmount+=item.amount
+            }
+            if(item.category=="Utilities"){
+                utilitiesAmount+=item.amount
+            }
+            if(item.category=="Entertainment"){
+                entertainmentAmount+=item.amount
+            }
+            if(item.category=="Others"){
+                othersAmount+=item.amount
+            }
+            allAmount+=item.amount
+            rentLabel.text=String(rentAmount)
+            groceriesLabel.text=String(groceriesAmount)
+            transportLabel.text=String(transportAmount)
+            utilitiesLabel.text=String(utilitiesAmount)
+            entertainmentLabel.text=String(entertainmentAmount)
+            othersLabel.text=String(othersAmount)
+        }
         
+    }
+    
+    @IBAction func allButton(_ sender: UIButton) {
         selectedCategory = "All";
-        
-        // perform segue programmatically
-        // move to the dashboard when "sign in" button is clicked
         self.performSegue(withIdentifier: "goTodisplayCategory", sender: nil);
     }
     
     @IBAction func rentButton(_ sender: UIButton) {
         selectedCategory = "Rent";
-        
-        // perform segue programmatically
-        // move to the dashboard when "sign in" button is clicked
         self.performSegue(withIdentifier: "goTodisplayCategory", sender: nil);
     }
     
     @IBAction func groceriesButton(_ sender: UIButton) {
         selectedCategory = "Groceries";
-        
-        // perform segue programmatically
-        // move to the dashboard when "sign in" button is clicked
         self.performSegue(withIdentifier: "goTodisplayCategory", sender: nil);
     }
     
     @IBAction func transportButton(_ sender: UIButton) {
         selectedCategory = "Transportation";
-        
-        // perform segue programmatically
-        // move to the dashboard when "sign in" button is clicked
         self.performSegue(withIdentifier: "goTodisplayCategory", sender: nil);
     }
     
-    @IBAction func utilitiesButton(_ sender: Any) {
+    @IBAction func utilitiesButton(_ sender: UIButton) {
         selectedCategory = "Utilities";
-        
-        // perform segue programmatically
-        // move to the dashboard when "sign in" button is clicked
         self.performSegue(withIdentifier: "goTodisplayCategory", sender: nil);
     }
     
-    @IBAction func entertainmentButton(_ sender: Any) {
+    @IBAction func entertainmentButton(_ sender: UIButton) {
         selectedCategory = "Entertainment";
-        
-        // perform segue programmatically
-        // move to the dashboard when "sign in" button is clicked
         self.performSegue(withIdentifier: "goTodisplayCategory", sender: nil);
     }
     
-    @IBAction func othersButton(_ sender: Any) {
+    @IBAction func othersButton(_ sender: UIButton) {
         selectedCategory = "Others";
-        
-        // perform segue programmatically
-        // move to the dashboard when "sign in" button is clicked
         self.performSegue(withIdentifier: "goTodisplayCategory", sender: nil);
     }
     
-    @IBAction func backButton(_ sender: Any) {
-        // perform segue programmatically
-        self.performSegue(withIdentifier: "returnToDash", sender: nil);
-    }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "goTodisplayCategory" {
-            let destinationVC = segue.destination as! displayCategoryViewController
-            destinationVC.categoryTitle = selectedCategory;
-            destinationVC.username = self.username;
-        }
-        else if segue.identifier == "returnToDash" {
-            let destinationVC = segue.destination as! dashboardViewController
-            destinationVC.username = self.username;
+    if segue.identifier=="goTodisplayCategory" {
+        let VC=segue.destination as! displayCategoryViewController
+        
+        VC.categoryTitle = selectedCategory;
+        
+        // pass username
+        // filter category
         }
     }
     
